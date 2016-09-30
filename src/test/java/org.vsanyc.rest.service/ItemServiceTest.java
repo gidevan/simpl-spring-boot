2c82638160ae3c85367db89e0756029338f69aa3
ipukhov <ivan.pukhov@cogniteq.com>
2016-09-30 18:03:51 +0300

    Master branch test was added

--- src/test/java/org.vsanyc.rest.service/ItemServiceTest.java
+++ src/test/java/org.vsanyc.rest.service/ItemServiceTest.java
@@ -10,11 +10,19 @@ import java.util.List;
 
 public class ItemServiceTest {
 
+    private static final int TEST_ID = 1;
+
     private ItemService itemService = new ItemServiceImpl();
 
     @Test
     public void testGetItems() {
         List<Item> items = itemService.getItems();
         Assert.assertFalse(items.isEmpty());
+    }
+
+    @Test
+    public void testById() {
+        Item item = itemService.getIemById(TEST_ID);
+        Assert.assertNotNull(item);
     }
 }
