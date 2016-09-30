544d848665725d9a735b8531ec117599b4916474
ipukhov <ivan.pukhov@cogniteq.com>
2016-09-30 17:36:20 +0300

    Test branch commit

--- src/test/java/org.vsanyc.rest.service/ItemServiceTest.java
+++ src/test/java/org.vsanyc.rest.service/ItemServiceTest.java
@@ -10,11 +10,19 @@ import java.util.List;
 
 public class ItemServiceTest {
 
+    private static final int TEST_ID = 0;
+
     private ItemService itemService = new ItemServiceImpl();
 
     @Test
     public void testGetItems() {
         List<Item> items = itemService.getItems();
         Assert.assertFalse(items.isEmpty());
+    }
+
+    @Test
+    public void testGetById() {
+        Item item = itemService.getIemById(TEST_ID);
+        Assert.assertNotNull(item);
     }
 }
